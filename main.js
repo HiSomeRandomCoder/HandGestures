fb1fa99ae5efc0462117915f9ed45ba58a6fd06e
Someone <91122743+HiSomeRandomCoder@users.noreply.github.com>
2024-02-12 18:14:33 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
 camera = document.getElementByyId("camera")
 
 Webcam.attach( '#camera' )
@@ -14,4 +21,44 @@ classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/
 function modelLoaded() {
     console.log('Model Loaded!');
     
+}
+
+function check () {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (errror) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("gesture-name").innerHTML = results[0].label;
+        classify.gotResult = results[0].label;
+        speak();
+        
+        if(results[0].label =="OK")
+        {
+document.getElementById("gesture-icon").innerHTML = "&#128076;";
+        }
+
+        if(results[0].label =="Thumbs Up")
+        {
+document.getElementById("gesture-icon").innerHTML = "&#128077;";
+        }
+
+        if(results[0].label =="Peace")
+        {
+document.getElementById("gesture-icon").innerHTML = "&#9996;";
+        }
+
+       
+    }
+}
+
+function speak () {
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The hand gesture is " + classify.gotResult;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1);
+    synth.speak(utterThis);
 }
