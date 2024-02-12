f91320d1d0c7ee7b63687b52c47cb762915f309d
Someone <91122743+HiSomeRandomCoder@users.noreply.github.com>
2024-02-12 17:14:54 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+camera = document.getElementByyId("camera")
+
+Webcam.attach( '#camera' )
+
+function captureImage() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/qhqMFf_P-/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+    
+}
